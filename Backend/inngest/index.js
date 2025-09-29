65afe332e7b136a56b0584d1ea852b46030f1427
kamalpatnaik <ptnayakunikamalakar@gmail.com>
2025-09-30 00:47:21 +0530

    new

--- Backend/inngest/index.js
+++ Backend/inngest/index.js
@@ -6,27 +6,31 @@ export const inngest = new Inngest({ id: "RConnect" });
 
 // inngest function to save user to db
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
-    async ({event})=>{
-        const{id, first_name, last_name, email_addresses, image_url}= event.data
-        let username = email_addresses[0].email_address.split('@')[0]
-        //check availabity of username
-        const user = await User.schema.findOne({username})
+  { id: 'sync-user-from-clerk' },
+  { event: 'clerk/user.created' },
+  async ({ event }) => {
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } = event.data;
+      let username = email_addresses[0].email_address.split('@')[0];
 
-        if(user){
-            username = username+ Math.floor(Math.random() * 10000)
-        }
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            full_name: first_name + " " + last_name,
-            profile_picture: image_url,
-            username
-        }
-        await User.create(userData)
+      const existingUser = await User.findOne({ username });
+      if (existingUser) username += Math.floor(Math.random() * 10000);
+
+      const userData = {
+        _id: id,
+        email: email_addresses[0].email_address,
+        full_name: `${first_name} ${last_name}`,
+        profile_picture: image_url,
+        username
+      };
+
+      await User.create(userData);
+      console.log('User created:', userData);
+    } catch (err) {
+      console.error('Error creating user:', err);
     }
-)
+  }
+);
 
 // inngest function to update user to db
 const syncUserUpdation = inngest.createFunction(
